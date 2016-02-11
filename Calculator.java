c580c29aeff14d7a8198f76f5588315d4c460166
shadoof42 <guravl42@yandex.ru>
2016-02-11 19:01:16 +0400

    interract Calculate

--- Calculator.java
+++ Calculator.java
@@ -5,15 +5,50 @@ public class Calculator{
 	/**
 		Результат вычисления.
 	*/
-	private int result;
+	private int result=0;
+	/**
+		Признак нетронутости результата вычислений.
+	*/
+	private boolean firstAction=true;
 	/**
 		Сумма всех аргументов.
 		@param params Аргументы суммирования.
 	*/
-	public void add(int ... params){
+	public void add(final int ... params){
 		for(Integer param : params){
 			this.result+=param;
 		}
+		if(firstAction)firstAction=false;
+	}
+	/**
+		Вычитание аргументов
+		@param params Аргументы вычитания.
+	*/
+	public void sub(final int ... params){
+		for(Integer param : params){
+			this.result-=param;
+		}
+		if(firstAction)firstAction=false;
+	}
+	/**
+		Умножение аргументов
+		@param params Аргументы умножения.
+	*/
+	public void mult(final int ... params){
+		for(Integer param : params){
+			this.result*=param;
+		}
+		if(firstAction)firstAction=false;
+	}
+	/**
+		Деление аргументов нацело
+		@param params Аргументы деления.
+	*/
+	public void div(final int ... params){
+		for(Integer param : params){
+			this.result=this.result/param;
+		}
+		if(firstAction)firstAction=false;
 	}
 	/**
 		Получить результат.
@@ -23,9 +58,17 @@ public class Calculator{
 		return this.result;
 	}
 	/**
+		Получить признак нетронутого результата
+		@return признак 
+	*/
+	public boolean getFirstAction(){
+		return this.firstAction;
+	}
+	/**
 		Очистить результат вычисления.
 	*/
 	public void clearResult(){
 		this.result = 0;
+		firstAction=true;
 	}
 }
--- InteractRunner.java
+++ InteractRunner.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 	Класс для запуска калькулятора. Поддерживает ввод пользователя.
 */
 public class InteractRunner{
-	
+	protected static Scanner reader = new Scanner(System.in);//Общий сканнер
+	protected Calculator calc = new Calculator();
+	/*
 	public static void main(String[] arg){
 		Scanner reader = new Scanner(System.in);
 		try{
@@ -12,12 +14,77 @@ public class InteractRunner{
 			while (!exit.equals("yes")){
 				System.out.println("Enter first arg : ");
 				String first = reader.next();
-				System.out.println("Enter second arg : ");
-				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
+				System.out.println("Enter next arg : ");
+				String next = reader.next();
+				calc.add(Integer.valueOf(first), Integer.valueOf(next));
 				System.out.println("Result : " + calc.getResult());
 				calc.clearResult();
 				System.out.println("Exit : yes/no ");
+				exit = reader.next();
+			}
+		}finally{
+			reader.close();
+		}
+	}
+	*/
+	/**
+		Выполнение операции.
+	*/
+	private void perform(final int next,final String action){
+		//System.out.println("Action:" + action);
+		switch(action){
+			case "+":
+				calc.add(next);		
+				break;
+			case "-":
+				calc.sub(next);
+				break;
+			case "*":
+				calc.mult(next);
+				break;
+			case "/":
+				calc.div(next);
+				break;
+			case "clear":
+				calc.clearResult();
+				break;
+			default:
+				System.out.println("No such operation! Please try again))))");
+				break;
+		}
+	}	
+	/**
+		Получение операндов и операции от пользователя.
+	*/
+	private String[] getParams(){
+		String[] params = new String[3];
+		//Проверяем, начинали ли мы работать  
+		if(calc.getFirstAction()){
+			System.out.println("Enter first arg : ");
+			calc.add(Integer.valueOf(reader.next()));
+		}else System.out.println("Previous result = " + calc.getResult());
+		params[0] = Integer.toString(calc.getResult());
+		System.out.println("Enter sign of action or 'clear' to clear result : ");
+		params[1] = reader.next();
+		if (!params[1].equals("clear")){
+			System.out.println("Enter second arg : ");
+			params[2] = reader.next();
+		}
+		return params;
+	}
+	
+	
+	public static void main(String[] arg){
+		InteractRunner runner = new InteractRunner();
+		try{
+			String exit = "no";
+			while (!exit.equals("yes")){
+				
+				String[] params=runner.getParams();
+				//System.out.println(params);
+				runner.perform(Integer.valueOf(params[2]),params[1]);
+				System.out.println("Result : " + runner.calc.getResult());
+				System.out.println("Exit? : yes/no ");
 				exit = reader.next();
 			}
 		}finally{
