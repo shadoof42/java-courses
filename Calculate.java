d40ed8d62ea43ce1881172ab31e1bb1bc037d016
shadoof42 <guravl42@yandex.ru>
2016-02-11 14:02:15 +0400

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,22 @@
+import java.lang.Math.*;
 public class Calculate{
-	public static void main (String[] arg){
-		System.out.println("Calculate");
+	public static void main (String... arg){
+		System.out.println("Calculate...");
+		if(arg[0]!=null&&arg[1]!=null){
+			double first = Double.valueOf(arg[0]);
+			double second = Double.valueOf(arg[1]);
+			double sum = first + second;
+			double sub = first - second;
+			double multi = first*second;
+			double div = first/second;
+			double power = Math.pow(first,second);
+			System.out.println("Sum = " + sum);
+			System.out.println("Sub = " + sub);
+			System.out.println("Multiplication = " + multi);
+			System.out.println("Division = " + div);
+			System.out.println("Power = " + power);
+		}else{
+			System.out.println("Enter two numbers!");
+		}
 	}
 }
